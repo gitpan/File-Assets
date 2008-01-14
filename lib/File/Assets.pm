ffb216b8490f274f8ab158a9063490037cb8890f
Robert Krimen <rkrimen@cpan.org>
2008-01-14 07:32:12 +0000

    Import of RKRIMEN/File-Assets-0.030 from CPAN.
    gitpan-cpan-distribution: File-Assets
    gitpan-cpan-version:      0.030
    gitpan-cpan-path:         RKRIMEN/File-Assets-0.030.tar.gz
    gitpan-cpan-author:       RKRIMEN
    gitpan-cpan-maturity:     released

--- lib/File/Assets.pm
+++ lib/File/Assets.pm
@@ -9,11 +9,11 @@ File::Assets - Manage .css and .js assets in a web application
 
 =head1 VERSION
 
-Version 0.021
+Version 0.030
 
 =cut
 
-our $VERSION = '0.021';
+our $VERSION = '0.030';
 
 =head1 SYNOPSIS
 
@@ -194,11 +194,14 @@ sub _export_html {
 <link rel="stylesheet" type="text/css" href="@{[ $asset->uri ]}" />
 _END_
         }
-        elsif ($asset->type->type eq "application/javascript") {
+        elsif ($asset->type->type eq "application/javascript" ||
+                $asset->type->type eq "application/x-javascript" || # Handle different MIME::Types versions.
+                $asset->type->type =~ m/\bjavascript\b/) {
             $html .= <<_END_;
 <script src="@{[ $asset->uri ]}" type="text/javascript"></script>
 _END_
         }
+
         else {
             $html .= <<_END_;
 <link type="@{[ $asset->type->type ]}" href="@{[ $asset->uri ]}" />
@@ -381,7 +384,7 @@ L<http://search.cpan.org/dist/File-Assets>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Robert Krimen, all rights reserved.
+Copyright 2008 Robert Krimen
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
